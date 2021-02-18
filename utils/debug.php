5526dc8ba1b859bfaeea2c9a92144b09b2e9290a
Łukasz Żmuda <l.t.zmuda@gmail.com>
2021-02-18 11:30:17 +0100

    create debug function in debug.php

--- utils/debug.php
+++ utils/debug.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
+error_reporting(E_ALL, 2047);
+ini_set("display_errors", 1);
+
+function dump($var)
+{
+    echo '<div style="display:inline-block;
+            padding: 10px;
+            border: 1px solid black;
+            background-color: gray;">
+            
+            <pre>';
+    print_r($var);
+    echo '</pre></div></br>';
+}
