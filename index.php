51a8f1f3d4105b84e191a1f873fe08bb9e901357
Łukasz Żmuda <l.t.zmuda@gmail.com>
2021-03-13 17:53:13 +0100

    create method escapeData in View class

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ namespace App;
 
 require_once("src/Controller.php");
 require_once("src/Request.php");
-require_once("src/Model/Database.php");
 require_once("src/utils/debug.php");
 require_once("src/Exceptions/ConfigException.php");
+require_once("src/Exceptions/AppException.php");
 
 use App\ConfigException;
 use App\AppException;
@@ -16,18 +16,20 @@ use App\Controller;
 use App\Request;
 use Throwable;
 
+
 $req = new Request($_GET, $_POST);
-
-
 
 try {
     $dbconfig = require_once("config/config.php");
-    Controller::initConfiguration($dbconfig);
+    AbstractController::initConfiguration($dbconfig);
     (new Controller($req))->run();
 } catch (AppException $e) {
     echo '<h1 style="text-align: center">' . $e->getMessage() . '</h1>';
+    dump($e);
 } catch (ConfigException $e) {
     echo '<h1 style="text-align: center">' . $e->getMessage() . '</h1>';
+    dump($e);
 } catch (Throwable $e) {
     echo '<h1 style="text-align: center">Wystąpił błąd w aplikacji</h1>';
+    dump($e);
 }
--- src/View.php
+++ src/View.php
@@ -9,6 +9,23 @@ class View
 
     public function renderSite(string $page, ?array $viewParams): void
     {
+        $viewParams = $this->escapeData($viewParams);
         require_once("templates/pages/layout.php");
+    }
+
+    private function escapeData(array $params): array
+    {
+        $clearParams = [];
+
+        foreach ($params as $key =>  $param) {
+            if (is_array($param)) {
+                $clearParams[$key] = $this->escapeData($param);
+            } else if ($param) {
+                $clearParams[$key] = htmlentities($param);
+            } else {
+                $clearParams[$key] = $param;
+            }
+        }
+        return $clearParams;
     }
 }
